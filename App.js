e7c0711bfaf5b8a14403ab49b75a2c54d8a4d48c
Lucas Caixeta <contato@lucascaixeta.com>
2016-09-06 14:59:37 +0100

    Using nested components - Was created a component with another component (button and icon)

--- App.js
+++ App.js
@@ -1,53 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component{
-    constructor(){
-        super();
-        this.state = {
-            red:    0,
-            green:  0,
-            blue:   0
-        }
-        this.update = this.update.bind(this)
-    }
-
-    update(e){
-        this.setState({
-            red:    ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-            green:  ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-            blue:   ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-        });
-    }
 
     render(){
-        return (
-            <div>
-               <Slider ref="red"    update={this.update}/>
-                {this.state.red}
-                <br/>
-               <Slider ref="green"  update={this.update}/>
-                {this.state.green}
-                <br/>
-               <Slider ref="blue"   update={this.update}/>
-                {this.state.blue}
-                <br/>
-            </div>
-        )
+        return <Button>I <Heart /> React</Button>
+    }
+
+}
+
+class Button extends React.Component{
+    render(){
+        return <button>{this.props.children}</button>
     }
 }
 
-class Slider extends React.Component {
-    render(){
-        return (
-            <div>
-                <input ref="inp" type="range"
-                       min=""
-                       max="255"
-                       onChange={this.props.update} />
-            </div>
-        )
-    }
-}
+const Heart = () => <i className='material-icons'>favorite</i>
 
 export default App
